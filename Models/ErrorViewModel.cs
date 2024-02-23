5f8cdf91389660d3e9b872a65a3980081cc94efa
Rashmi Priya <priye98priya@gmail.com>
2024-02-23 17:17:14 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project3Phase4_prj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
